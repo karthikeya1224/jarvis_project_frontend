eec048184b3cedee374c4fb3f39cf8f7d041c2e3
varsha-tejaswi27 <varshatejaswi2004@gmail.com>
2026-06-17 09:51:41 +0530

    1st push

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider 
+    clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+  >
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </GoogleOAuthProvider>
 )
